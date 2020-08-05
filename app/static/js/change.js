798f55656560f3232a49936cf475f43ff70ae05f
kirari_kiis <kirari0709@icloud.com>
2020-08-05 14:15:29 +0900

    design1

--- app/static/js/change.js
+++ app/static/js/change.js
@@ -3,21 +3,5 @@ $(window).on('load', function(){
  $('body').removeClass('is-slide');
 });
 
-$(function() {
- // ハッシュリンク(#)と別ウィンドウでページを開く場合は実行しない
 
- $('a:not([href^="#"]):not([target])').on('click', function(e){
-   e.preventDefault();         // ページ遷移を一旦キャンセル
-   url = $(this).attr('href'); // 遷移先のURLを取得
-
-   if (url !== '') {
-     $('body').addClass('is-slide-in'); // 画面遷移前のアニメーション is-slide-in
-
-     setTimeout(function () {
-       window.location = url;  // 0.7秒後に取得したURLに遷移
-     }, 700);
-   }
-   return false;
- });
-
-});
+ 
--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,5 +1,61 @@
-//ヘッダーの高さ分だけコンテンツを下げる
-$(function() {
-    var height=$("#header").height();
-    $("body").css("margin-top", height + 10);//10pxだけ余裕をもたせる
-});
+// //ヘッダーの高さ分だけコンテンツを下げる
+// $(function() {
+//     var height=$("#header").height();
+//     $("body").css("margin-top", height + 10);//10pxだけ余裕をもたせる
+// });
+
+
+// //リンク先までスクロール
+// $(function(){
+
+//     // htmlとbody、どちらかスクロール可能な要素を取得
+//     function getFirstScrollable(selector){
+//       var $scrollable;
+  
+//       $(selector).each(function(){
+//         var $this = $(this);
+//         if( $this.scrollTop() > 0 ){
+//           $scrollable = $this;
+//           return false;
+//         }else{
+//           $this.scrollTop(1);
+//           if( $this.scrollTop() > 0 ){
+//             $scrollable = $this;
+//             return false;
+//           }
+//           $this.scrollTop(0);
+//         }
+//       });
+  
+//       return $scrollable;
+//     }
+  
+//     // スクロールに使用する要素・イベントを設定
+//     var $win = $(window),
+//         $doc = $(document),
+//         $scrollElement = getFirstScrollable("html,body"),
+//         mousewheel = "onwheel" in document ? "wheel" : "onmousewheel" in document ? "mousewheel" : "DOMMouseScroll";
+  
+//     // aタグのクリック
+//     $doc.on("click", "a[href^='#']", function(e){
+//       var $target = $(this.hash),
+//           top;
+  
+//       // 指定した要素が存在しない場合は未処理とする
+//       if( $target.size() < 1 ) return false;
+  
+//       // スクロール先の座標を調整する
+//       top = $target.offset().top;
+//       top = Math.min(top, $doc.height() - $win.height());
+  
+//       // ウィールイベントをキャンセルしておく
+//       $doc.on(mousewheel, function(e){ e.preventDefault(); });
+  
+//       // アニメーションの実行
+//       $scrollElement.stop().animate({scrollTop: top}, 1000, "easeOutQuint", function(){
+//         $doc.off(mousewheel);
+//       });
+  
+//       return false;
+//     });
+//   });
